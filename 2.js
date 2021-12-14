c4d1ee063779fc1f2a7521ae6f34e548a76aba5c
Ri <ridraqon@internet.ru>
2021-12-14 14:15:45 +0300

    Практическая работа к 1 уроку.

---
+++ 2.js
@@ -0,0 +1,18 @@
+/*
+*2. Работа с переменными.
+*/
+"use strict";
+
+var user = 'Виктор';
+var age = 18;
+message = 'Hello' + 'World';
+
+alert('Имя: ' + user, 'Лет: ' + age, 'Сообщение от ' + user + message);
+
+var a = 5;
+var b = 7;
+var c = '7';
+x = a + b;
+alert(x);
+y = a + b + c;
+alert(y);
